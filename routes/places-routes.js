8d06e6a1101a654aef7f84425dbdd0bf7f0569a8
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-04-20 02:34:59 +0500

    update place logic complete for now

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -12,6 +12,8 @@ router.get('/user/:uid', placeControllers.getPlaceByUserId);
 
 router.post('/', placeControllers.createPlace);
 
+router.patch('/:pid', placeControllers.updatePlace);
 
+router.delete('/:pid', placeControllers.deletePlace);
 
 module.exports = router;
