cf529428e698c1fab7f7944c3f7fbc879d8bf521
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-05-02 23:38:37 +0500

    update place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -116,7 +116,7 @@ const createPlace = async (req, res, next) => {
 };
 
 // 4th function middleWare
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log('errors ==>', errors)
@@ -126,14 +126,34 @@ const updatePlace = (req, res, next) => {
     const { title, description } = req.body;
     const placeId = req.params.pid;
 
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
-    updatedPlace.title = title;
-    updatedPlace.description = description;
+    let place;
+    try {
+        place = await Place.findById(placeId);
+    } catch (err) {
+        const error = new HttpError('Something went wrong while updating a place, please try again later!', 500)
+        return next(error);
+    }
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
+    place.title = title;
+    place.description = description;
 
-    res.status(200).json({ place: updatedPlace });
+    try {
+        await place.save();
+    } catch (err) {
+        const error = new HttpError('Something went wrong while updating a place, please try again later!', 500)
+        return next(error);
+    }
+
+    // using this logic for only when there was no database here!
+    // const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+    // const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
+    // updatedPlace.title = title;
+    // updatedPlace.description = description;
+    // DUMMY_PLACES[placeIndex] = updatedPlace;
+
+    res.status(200).json({
+        place: place.toObject({ getters: true })
+    });
 
 };
 
