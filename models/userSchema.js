aafce1209ca631dee9515703c9b9fc8752bc3764
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-05-08 23:24:49 +0500

    userSchema

---
+++ models/userSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    images: { type: String, required: true },
+    places: { type: String, required: true },
+});
+
+// Don't add userSchema.plugin(uniqueValidator);
+// instructor using mongoose-unique-validator but deprecated in newer versions there unique:true will handle the same work
+
+module.exports = mongoose.model('User', userSchema);
+
+
