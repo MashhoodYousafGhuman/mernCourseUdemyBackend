0279385c60cfb49fa89f0f4401e60bd28914b7f8
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-05-31 02:43:51 +0500

    removing file from git

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
     if (req.file) {
         fs.unlink(req.file.path, (err) => {
-            console.log('err', err);
+            console.log('err while unlinking file in app.js', err)
         });
     }
 
