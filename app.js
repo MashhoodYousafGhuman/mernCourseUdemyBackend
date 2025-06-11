0ad2418b1ec828e86acc2796c3870f17fb9005a1
MashhoodYousafGhuman <mashhoodyousaf24@gmail.com>
2025-06-12 01:39:42 +0500

    little comment for memoryCall

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+// app.use(
+//     // this line will be used if we want to deploy our fEnd and bEnd on same server;
+//     express.static(path.join('public'))
+// );
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,15 +34,20 @@ app.use((req, res, next) => {
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
+// app.use((req, res, next) => {
+// this line will be used if we want to deploy our fEnd and bEnd on same server;
+//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// })
+
 app.use((req, res, next) => {
     const error = new HttpError('Could not find this route', 404);
     throw error;
 });
 
 
-// middleWare for sending errors, like a errorAPI , this middleWare will run or apply  on every function or middleWare that have error attached to it
 
 app.use((error, req, res, next) => {
+    // middleWare for sensding errors, like a errorAPI , this middleWare will run or apply  on every function or middleWare that have error attached to it
     if (req.file) {
         fs.unlink(req.file.path, (err) => {
             console.log('err while unlinking file in app.js', err)
@@ -55,7 +64,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER_NAME}.0ixtjhc.mongodb.net/`)
     .then(() => {
-        app.listen(5000)
+        app.listen(process.env.PORT || 5000)
         console.log('Database Connected');
         console.log('server is running');
     })
